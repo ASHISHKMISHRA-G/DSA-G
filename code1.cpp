39cfd1c17b4eed09b743e38b9b648d5197537471
unknown <you@example.com>
2022-12-10 18:33:10 +0530

    code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -1,2 +1,34 @@
-N = 6
-A[] = {3, 2, 1, 56, 10000,};
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findMinAndMax(vector<int> const &nums, int &min, int &max)
+{
+	max = nums[0], min = nums[0];
+
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (nums[i] > max) {
+			max = nums[i];
+		}
+
+		else if (nums[i] < min) {
+			min = nums[i];
+		}
+	}
+}
+
+int main()
+{
+	vector<int> nums = { 5, 23,6,8,12};
+	int min, max;
+
+	findMinAndMax(nums, min, max);
+
+	cout << "The minimum array element is " << min << endl;
+	cout << "The maximum array element is " << max << endl;
+
+	return 0;
+}
